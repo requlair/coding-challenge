435866845f1d01ecf050531b8c557300216f504b
Lejon Bergman <lejon.bergman@nn-group.com>
2022-12-06 16:56:39 +0100

    Initialized Vue app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div>
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
