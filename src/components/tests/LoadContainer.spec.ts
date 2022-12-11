c4c2f6b0ac90b0ea58e21eb2469bb18da9fbc365
Lejon Bergman <lejon.bergman@nn-group.com>
2022-12-11 12:49:21 +0100

    Added test

---
+++ src/components/tests/LoadContainer.spec.ts
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, it, vi } from 'vitest';
+import LoadContainer from '../LoadContainer.vue';
+
+describe('Load container component', () => {
+  it('should render with spinner when status is loading', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.findAll('.lds-ring').length).toBe(3);
+  });
+  it('should render with content when status is done', () => {
+    const wrapper = createWrapper({ status: 'done', variant: 'card'});
+    expect(wrapper.find('.lds-ring').exists()).toBeFalsy();
+    expect(wrapper.text()).toBe('Content');
+  })
+});
+
+const createWrapper = (props = { status: 'loading', variant: 'card'}) => {
+  return mount(LoadContainer, {
+    props: props,
+    slots: {
+        default: 'Content'
+    }
+  });
+};
